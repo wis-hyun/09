47839b8cfb2fe403bb3c4ab886f585e7f8a3d007
김성현 <khyun8576@naver.com>
2023-10-30 20:55:29 +0900

    2-2

--- main.c
+++ main.c
@@ -6,13 +6,18 @@
 
 int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
-	int i;
+	int i, average;
+	int sum;
 	printf("input 5 scores: ");
 	
+	sum=0;
 	for(i=0; i<N_STUDENT; i++)
+	{
 		scanf("%d", &grade[i]);
+		sum += grade[i];
+	}
 	
-	for(i=0; i<N_STUDENT; i++)
-		printf("grade[%d]=%d\n", i, grade[i]);
+	average = sum / N_STUDENT;
+	printf("score average : %i\n", average);
 	return 0;
 } 
