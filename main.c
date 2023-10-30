e00ec5a748875f1cd14d987cc7de7c9836fb7335
김성현 <khyun8576@naver.com>
2023-10-30 21:14:13 +0900

    3-2

--- main.c
+++ main.c
@@ -5,20 +5,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int grade[N_STUDENT];
+	
 	int i;
-	int score[N_STUDENT];
+	int a[N_STUDENT]={1,2,3,4,5};
+	int b[N_STUDENT]={1,2,3,4,5};
+	int flag =0;
 	
-
-	for(i=0; i<N_STUDENT; i++)
-		grade[i] = rand()%100 +1;
-
-	for(i=0; i<N_STUDENT; i++)
-		score[i]=grade[i];
-
-		
-	for(i=0; i<N_STUDENT; i++)
-		printf("score[%d]= %d (grade : %i)\n", i, score[i], grade[i]);
+	for (i=0; i<N_STUDENT; i++){
 	
+		if(a[i]!=b[i])
+		{
+			printf("array a and b are not the same\n");
+			flag =1;
+		}
+	}
+	
+	if (flag == 0)
+		printf("array a  and b are the same\n");
 	return 0;
 } 
