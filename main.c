c28ccebaca38d4c29f00ce4fb314809db0581516
김성현 <khyun8576@naver.com>
2023-10-30 20:51:20 +0900

    2-1

--- main.c
+++ main.c
@@ -7,14 +7,12 @@
 int main(int argc, char *argv[]) {
 	int grade[N_STUDENT];
 	int i;
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
+	printf("input 5 scores: ");
 	
 	for(i=0; i<N_STUDENT; i++)
-		printf("grade[%i]=%i\n", i, grade[i]);
+		scanf("%d", &grade[i]);
 	
+	for(i=0; i<N_STUDENT; i++)
+		printf("grade[%d]=%d\n", i, grade[i]);
 	return 0;
 } 
